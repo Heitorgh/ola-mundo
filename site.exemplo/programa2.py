dd36ad6d91272de21317c10706a07db6d2a35692
Heitor da Silva Garcia <heitorgarciatr@gmail.com>
2024-11-14 16:12:02 -0300

    Update programa2.py

--- site.exemplo/programa2.py
+++ site.exemplo/programa2.py
@@ -9,5 +9,10 @@ pa = input('Digite a palavra que você quer substituir: ')
 pn = input('Digite a palavra nova: ')
 lista= frase.replace(pa, pn)
 print(lista)
+frase_inversa = ' '. join(lista.split()[::-1])
+print(frase_inversa)
+cont = input('Digite a letra que você quer contar: ')
+cont2 = lista.count(cont)
+print(cont2)
 
 
