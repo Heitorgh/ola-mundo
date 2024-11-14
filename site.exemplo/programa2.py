7f35d263752b99c055ff0abdf486e59aa611af9d
Heitor da Silva Garcia <heitorgarciatr@gmail.com>
2024-11-13 21:52:42 -0300

    Update programa2.py

--- site.exemplo/programa2.py
+++ site.exemplo/programa2.py
@@ -1,9 +1,13 @@
 lista = []
 frase = input('Digite sua frase: ')
+frase.split()
+qtd = len(frase.split())
 lista.append(frase)
+print(qtd)
 print(lista)
 pa = input('Digite a palavra que você quer substituir: ')
 pn = input('Digite a palavra nova: ')
 lista= frase.replace(pa, pn)
 print(lista)
+
 
