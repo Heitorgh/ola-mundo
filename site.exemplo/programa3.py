131c75634ad1d5c5ebf3c68cc6e8034b36183152
Heitor da Silva Garcia <heitorgarciatr@gmail.com>
2024-11-29 21:24:09 -0300

    Update programa3.py

--- site.exemplo/programa3.py
+++ site.exemplo/programa3.py
@@ -1,11 +1,17 @@
-ListaDeNotas= []
 Maximo = 0
 Minimo = 10
+SDN = 0
+NN = 0
+SQ = 0
+
 
 Notas = float(input('Entre com uma nota de 0 a 10: '))
 
 while 0 <= Notas <= 10 :
-    ListaDeNotas.append(Notas)
+    SDN += Notas
+    NN += 1
+    SQ += Notas **2
+
 
     if Notas > Maximo:
     
@@ -14,10 +20,21 @@ while 0 <= Notas <= 10 :
     if Notas < Minimo:
 
         Minimo = Notas
+    
 
 
     Notas = float(input('Entre com outra nota de 0 a 10: '))
 
-print(ListaDeNotas)
-print('A nota Máxima é: ', Maximo)
-print('A nota Mínima é: ', Minimo)
+if NN > 0:
+    Media = SDN / NN
+
+    V = (SQ / NN) - (Media ** 2)
+
+    DP = V ** 0.5 
+ 
+print(f'Soma das Notas: {SDN}')
+print(f'Número de Notas: {NN}')
+print(f'A média dos alunos é: {Media:.2f}')
+print(f'A nota Máxima é: {Maximo}')
+print(f'A nota Mínima é: {Minimo}')
+print(f'O desvio padrão é: {DP:.2f}')
